c46dbb8c50761c6005949ed37571f08ad7849d20
sb <114514@jb.com>
2026-05-07 22:34:29 +0800

    完成pdf转文本

---
+++ src/services/ocr_pdf_to_text.py
@@ -0,0 +1,25 @@
+from paddleocr import PaddleOCR
+
+ocr = PaddleOCR(
+    use_angle_cls=True,
+    lang="ch",
+    device="gpu"
+)
+
+def pdf_to_text(path: str) -> str:
+
+
+    results = ocr.predict("./static/test2.pdf")
+
+    all_text = []
+
+    for res in results:
+
+        texts = res['rec_texts']
+
+        all_text.extend(texts)
+
+    final_text = "\n".join(all_text)
+
+    print(final_text)
+    return final_text
---
+++ tests/test_markdown.py
@@ -0,0 +1,21 @@
+from paddleocr import PaddleOCR
+
+ocr = PaddleOCR(
+    use_angle_cls=True,
+    lang="ch",
+    device="gpu"
+)
+
+results = ocr.predict("./static/test2.pdf")
+
+all_text = []
+
+for res in results:
+
+    texts = res['rec_texts']
+
+    all_text.extend(texts)
+
+final_text = "\n".join(all_text)
+
+print(final_text)
