ee75b558f1a010601dcaf0a7d902b766a531b42d
sb <114514@jb.com>
2026-05-07 22:51:45 +0800

    test_commit

--- src/services/ocr_pdf_to_text.py
+++ src/services/ocr_pdf_to_text.py
@@ -7,19 +7,11 @@ ocr = PaddleOCR(
 )
 
 def pdf_to_text(path: str) -> str:
-
-
     results = ocr.predict("./static/test2.pdf")
-
     all_text = []
-
     for res in results:
-
         texts = res['rec_texts']
-
         all_text.extend(texts)
-
     final_text = "\n".join(all_text)
-
     print(final_text)
     return final_text
--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -1,3 +1,4 @@
+import json
 from paddleocr import PaddleOCR
 
 ocr = PaddleOCR(
@@ -8,14 +9,54 @@ ocr = PaddleOCR(
 
 results = ocr.predict("./static/test2.pdf")
 
-all_text = []
+documents = []
 
-for res in results:
+for page_idx, res in enumerate(results):
 
-    texts = res['rec_texts']
+    texts = res.get("rec_texts", [])
 
-    all_text.extend(texts)
+    # 清洗文本
+    clean_texts = []
 
-final_text = "\n".join(all_text)
+    for text in texts:
 
-print(final_text)
+        text = str(text).strip()
+
+        # 过滤空行
+        if not text:
+            continue
+
+        # 过滤超短乱码
+        if len(text) == 1:
+            continue
+
+        clean_texts.append(text)
+
+    page_text = "\n".join(clean_texts)
+
+    documents.append({
+        "page": page_idx + 1,
+        "text": page_text
+    })
+
+# 整本文档
+full_text = "\n\n".join(
+    [doc["text"] for doc in documents]
+)
+
+print(full_text)
+
+# 保存 JSON
+with open("output.json", "w", encoding="utf-8") as f:
+
+    json.dump(
+        {
+            "full_text": full_text,
+            "pages": documents
+        },
+        f,
+        ensure_ascii=False,
+        indent=2
+    )
+
+print("OCR完成，已保存 output.json")
